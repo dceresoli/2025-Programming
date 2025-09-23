3120c89686351dbb5dfe26ad2b2628414b84eb8d
Davide Ceresoli <dceresoli@gmail.com>
2025-09-23 15:23:48 +0200

    lec2 and lec3 done

---
+++ lec02/integer.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+   unsigned char a = 170;
+   unsigned char two_a = a * 2;
+
+   printf("a = %i\n", a);
+   printf("2*a = %i\n", two_a);
+
+   return 0;
+}
